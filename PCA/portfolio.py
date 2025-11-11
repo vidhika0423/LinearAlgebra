f36beebebdf5a1579971600e7f526adc90f16caf
vidhika0423 <vidhika0423@gmail.com>
2025-11-11 21:17:36 +0530

    updated plots

--- PCA/portfolio.py
+++ PCA/portfolio.py
@@ -21,7 +21,7 @@ tickers = [
 start = '2020-01-01'
 end = datetime.today().strftime('%Y-%m-%d')
 
-prices = yf.download(tickers, start=start, end=end)
+prices = yf.download(tickers, start=start, end=end)['Close']
 prices = prices.dropna()    #drop if any ticker is missing
 
 # simple portfolio
@@ -54,12 +54,12 @@ scores = pd.DataFrame(pca_model.transform(X), index=X.index,
 port_returns = (returns * weights).sum(axis=1)
 
 # portfolio exposure to each PC = weights dot loadings for that PC
-portfolio_exposure = loadings.multiply(weights, axiz=0).sum(axis=0)
+portfolio_exposure = loadings.multiply(weights, axis=0).sum(axis=0)
 
 # remove 1st PC from returns
 pc1 = pca_model.components_[0]
 
-pc1_series = scores['PC!']      # time series of PC1
+pc1_series = scores['PC1']      # time series of PC1
 
 # contribution of PC! to each asset at each time = outer product of PC! and pc1_series
 asset_contributution_pc1 = pd.DataFrame(np.outer(pc1_series.values, pc1), index=X.index, columns=tickers)
@@ -79,3 +79,26 @@ print("Annualized volatility (after removing PC1):", vol_after)
 print(f"PC1 explains {explained_var[0]*100:.1f}% of variance (first PC).")
 print("\nTop loadings for PC1 (assets most correlated with PC1):")
 print(loadings['PC1'].abs().sort_values(ascending=False).head(6))
+
+
+# plots
+
+plt.figure(figsize=(8,4))
+plt.plot(cum_explained[:10]*100, marker='o')
+plt.title("Cumulativr variance explained")
+plt.xlabel('Number of PC')
+plt.ylabel('variance explained(%)')
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(8,4))
+loadings['PC1'].sort_values(ascending=True).plot(kind='bar')
+plt.title("pc1 Loadings")
+plt.show()
+
+plt.figure(figsize=(8,4))
+plt.plot(port_returns.cumsum(), label='original')
+plt.plot(port_returns_neutral.cumsum(), label='pc1 neutralized')
+plt.legend()
+plt.title("Cumulative Returns: Original vs Neutralized Portfolio")
+plt.show()
