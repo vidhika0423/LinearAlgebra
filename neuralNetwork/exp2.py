865877064b69ddc68040a7b7bb03fb6fc5b04e47
vidhika0423 <vidhika0423@gmail.com>
2025-11-14 21:02:46 +0530

    neural network layer

--- neuralNetwork/exp2.py
+++ neuralNetwork/exp2.py
@@ -25,3 +25,25 @@ output = [inputs[0]*weights1[0] +  inputs[1]*weights1[1] +  inputs[2]*weights1[2
           inputs[0]*weights3[0] +  inputs[1]*weights3[1] +  inputs[2]*weights3[2]+  inputs[3]*weights3[3] + bias3]
 print(output)
 
+# method 2
+# code a neuron layer
+inputs = [1, 2, 3, 2.5]
+
+weights = [[0.2, 0.8, -0.5, 0.1],
+           [0.5, -0.91, -0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+layer_outputs = []
+
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input*weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+
+print(layer_outputs)
+
